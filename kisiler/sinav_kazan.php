e8f7750b082f3e151ce2b28d1049d9f58f72b8df
Erhan Mutluay <mutluayerhan@gmail.com>
2022-12-28 21:18:05 +0300

    adadsads

--- kisiler/sinav_kazan.php
+++ kisiler/sinav_kazan.php
@@ -1,13 +1,24 @@
 <?php
 
-$baglan = mysqli_connect("localhosst", "root", "", "surucu_kurs");
+$baglanti = new mysqli("localhost", "root", "", "surucu_kurs");
 
-$sorgu = $baglan->query("SELECT isim_soyisim, dogum_tarihi FROM sinavi_kazan");
+if ($baglanti->connect_errno > 0) {
+    die("<b>Bağlantı Hatası:</b> " . $baglanti->connect_error);
+}
+
+$baglanti->set_charset("utf8");
+
+$sorgu = $baglanti->query("SELECT isim FROM hizmetlerimiz");
+
+if ($baglanti->errno > 0) {
+    die("<b>Sorgu Hatası:</b> " . $baglanti->error);
+}
+
 $cikti = $sorgu->fetch_array();
 
+echo "Adı: " . $cikti["isim"];
 
-
-
-
+$sorgu->close();
+$baglanti->close();
 
 ?>
